4d8bf652c0e03ffe2200af4b85fc36c09915bfab
xxraihann <raihanananda1980@gmail.com>
2026-07-21 15:19:07 +0700

    Menambahkan PWA

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "inventaris-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./api.js",
+  "./assets/logo-kai.png",
+  "./assets/header-bg.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
